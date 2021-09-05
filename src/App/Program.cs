ce839319ec3b58b706882811f3365f03f76cecc3
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2021-09-05 14:06:08 +0200

    Top-level-statements rule!

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -1,12 +1,3 @@
-﻿using System;
-
-namespace App
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿using Domain;
+var you = new Person();
+you.SayHello();
